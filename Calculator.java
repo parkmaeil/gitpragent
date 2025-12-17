f52c20b79165d399172efe97d8421de0dddae85a
parkmaeil <bitcocom@empas.com>
2025-12-17 14:02:36 +0900

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
     public int add(int a, int b) {
+        System.out.println("더하기 시작"); // 불필요한 로그
+        if (a == 10) return 0; // 이상한 버그 로직
         return a + b;
     }
 }
