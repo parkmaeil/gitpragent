35d100dafcebd841e0b0c78aaa20fb33f6901afa
parkmaeil <bitcocom@empas.com>
2025-12-18 16:01:40 +0900

    Change add method return type to String

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public void add(int a, int b) {
+    public String add(int a, int b) {
         System.out.println("더하기 시작"); 
         if (a == 10) return 0; 
         return a + b;
