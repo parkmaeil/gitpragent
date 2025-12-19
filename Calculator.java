a437e753a93cc3df5cde8cd4a1c455d6e1f7c7d6
parkmaeil <bitcocom@empas.com>
2025-12-19 17:18:26 +0900

    Fix add method to return int instead of Max

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
      * 과제: 두 정수 a와 b를 더한 값을 반환하는 메서드를 완성하세요.
      * 주의: 현재 코드는 버그가 있습니다.
      */
-    public Max add(int a, int b) {
+    public int add(int a, int b) {
         int temp=a+b;
         return temp; 
     }
